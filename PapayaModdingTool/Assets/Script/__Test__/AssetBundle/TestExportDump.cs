0374bce7a16e28bd6b50517c731817f87ece0049
Kolyn090 <kolynlin@protonmail.com>
2025-07-29 22:39:36 -0400

    Renamed scene

--- PapayaModdingTool/Assets/Script/__Test__/AssetBundle/TestExportDump.cs
+++ PapayaModdingTool/Assets/Script/__Test__/AssetBundle/TestExportDump.cs
@@ -1,7 +1,14 @@
+using System;
+using PapayaModdingTool.Assets.Script.__Test__.TestUtil;
+using UnityEngine;
+
 namespace PapayaModdingTool.Assets.Script.__Test__.AssetBundle
 {
-    public class TestExportDump
+    public class TestExportDump : MonoBehaviour, ITestable
     {
-        
+        public void Test(Action onComplete)
+        {
+            
+        }
     }
 }
