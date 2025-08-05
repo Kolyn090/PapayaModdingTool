0ce78c66b1542d3f5fd99cac04c8a5625a7a70e1
Kolyn090 <kolynlin@protonmail.com>
2025-08-05 12:08:54 -0400

    Refactor - main window load files helper

---
+++ PapayaModdingTool/Assets/Script/Editor/TextureModding/MainWindowHelper/LoadFilesHelper.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using PapayaModdingTool.Assets.Script.DataStruct.FileRead;
+using PapayaModdingTool.Assets.Script.Editor.Writer.TextureModding;
+using PapayaModdingTool.Assets.Script.Misc.AppCore;
+using PapayaModdingTool.Assets.Script.Misc.Paths;
+using PapayaModdingTool.Assets.Script.Writer.ProjectUtil;
+using UnityEditor;
+using UnityEngine;
+
+namespace PapayaModdingTool.Assets.Script.Editor.TextureModding.MainWIndowHelper
+{
+    public class LoadFilesHelper
+    {
+        public Func<List<string>> GetLoadedPaths;
+        public Action<List<string>> SetLoadedPaths;
+        public Func<bool> GetLoadedPathsChanged;
+        public Action<bool> SetLoadedPathsChanged;
+        public Func<string> GetProjectName;
+        public Func<List<string>> LoadPathsFromDB;
+        public Func<TextureAssetsLoader> GetTextureAssetsLoader;
+        public Func<AppEnvironment> GetAppEnvironment;
+        public Func<string, string> ELT;
+        private Vector2 _scrollPos;
+        private readonly ProjectWriter _projectWriter = new();
+        private int _ppu = 100;
+
+        public void CreateLoadedPathsPanel()
+        {
+            GUIStyle rightAlignedStyle = new(EditorStyles.label)
+            {
+                alignment = TextAnchor.MiddleRight
+            };
+
+            _ppu = EditorGUILayout.IntField(ELT("ppu"), _ppu);
+
+            GUILayout.Space(20);
+
+            // Only update when necessary
+            if (GetLoadedPaths == null || GetLoadedPathsChanged())
+            {
+                SetLoadedPaths(LoadPathsFromDB());
+                SetLoadedPathsChanged(false);
+            }
+
+            // Scroll view for loaded paths
+            GUILayout.Label(ELT("loaded_files"), EditorStyles.boldLabel);
+            _scrollPos = EditorGUILayout.BeginScrollView(_scrollPos, GUILayout.Height(50));
+            foreach (var item in GetLoadedPaths())
+            {
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.SelectableLabel(item, rightAlignedStyle, GUILayout.Height(15));
+                if (GUILayout.Button(ELT("copy"), GUILayout.Width(50), GUILayout.Height(15)))
+                {
+                    EditorGUIUtility.systemCopyBuffer = item;
+                    Debug.Log("Copied: " + item);
+                }
+                EditorGUILayout.EndHorizontal();
+            }
+            EditorGUILayout.EndScrollView();
+            GUILayout.Space(5);
+            if (GUILayout.Button(ELT("load_new")))
+            {
+                LoadNewFile();
+            }
+        }
+
+        private void LoadNewFile()
+        {
+            LoadFileInfo? loadInfo = _projectWriter.LoadNewFile(GetProjectName(),
+                                        GetAppEnvironment().Wrapper.FileBrowser,
+                                        GetAppEnvironment().Wrapper.JsonSerializer,
+                                        LoadType.Texture);
+            if (loadInfo == null)
+                return;
+
+            AttemptToLoadTexture((LoadFileInfo)loadInfo);
+            SetLoadedPathsChanged(true);
+        }
+
+        private void AttemptToLoadTexture(LoadFileInfo loadInfo)
+        {
+            string textureDir = PredefinedPaths.PapayaTextureDir;
+            string textureSaveDir = Path.Combine(textureDir, GetProjectName(), loadInfo.folder);
+            if (!Directory.Exists(textureSaveDir))
+                Directory.CreateDirectory(textureSaveDir);
+            GetTextureAssetsLoader().LoadTextureAssets(loadInfo, GetProjectName(), textureSaveDir, _ppu);
+        }
+    }
+}
--- PapayaModdingTool/Assets/Script/Editor/TextureModding/TextureModdingMainWindow.cs
+++ PapayaModdingTool/Assets/Script/Editor/TextureModding/TextureModdingMainWindow.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.IO;
 using PapayaModdingTool.Assets.Script.DataStruct.FileRead;
+using PapayaModdingTool.Assets.Script.Editor.TextureModding.MainWIndowHelper;
 using PapayaModdingTool.Assets.Script.Editor.Universal;
 using PapayaModdingTool.Assets.Script.Editor.Writer.ProjectUtil;
 using PapayaModdingTool.Assets.Script.Editor.Writer.TextureModding;
@@ -8,7 +9,6 @@ using PapayaModdingTool.Assets.Script.Editor.Writer.Universal;
 using PapayaModdingTool.Assets.Script.Misc.Paths;
 using PapayaModdingTool.Assets.Script.Reader.ProjectUtil;
 using PapayaModdingTool.Assets.Script.Writer.AddressableTools;
-using PapayaModdingTool.Assets.Script.Writer.ProjectUtil;
 using UnityEditor;
 using UnityEngine;
 
@@ -17,17 +17,31 @@ namespace PapayaModdingTool.Assets.Script.Editor.TextureModding
     public class TextureModdingMainWindow : MainWindow
     {
         private readonly ProjectLoader _projectLoader = new();
-        private readonly ProjectWriter _projectWriter = new();
         private static ProjectRemover _projectRemover;
         private readonly TextureAssetsLoader _textureAssetsLoader = new(_appEnvironment);
-        private Vector2 _scrollPos;
         private string _removeLoadedPath;
         private string _installLoadedPath;
         private string _catalogPath;
-        private int _ppu = 100;
         private static List<string> _loadedPaths = null;
         private bool _loadedPathsChanged = true;
         private BuildTarget _buildPlatform = BuildTarget.StandaloneWindows64;
+
+        private LoadFilesHelper _loadFilesHelper;
+
+        private void OnEnable() {
+            _loadFilesHelper = new()
+            {
+                GetLoadedPaths = () => _loadedPaths,
+                SetLoadedPaths = var => _loadedPaths = var,
+                GetLoadedPathsChanged = () => _loadedPathsChanged,
+                SetLoadedPathsChanged = var => _loadedPathsChanged = var,
+                GetProjectName = () => ProjectName,
+                LoadPathsFromDB = () => _projectLoader.FindLoadedPaths(ProjectName, _appEnvironment.Wrapper.JsonSerializer),
+                GetTextureAssetsLoader = () => _textureAssetsLoader,
+                GetAppEnvironment = () => _appEnvironment,
+                ELT = var => ELT(var)
+            };
+        }
 
         public static void Open(string projectPath)
         {
@@ -42,41 +56,7 @@ namespace PapayaModdingTool.Assets.Script.Editor.TextureModding
         {
             base.OnGUI();
 
-            GUIStyle rightAlignedStyle = new(EditorStyles.label)
-            {
-                alignment = TextAnchor.MiddleRight
-            };
-
-            _ppu = EditorGUILayout.IntField(ELT("ppu"), _ppu);
-
-            if (_loadedPaths == null || _loadedPathsChanged)
-            {
-                _loadedPaths = _projectLoader.FindLoadedPaths(ProjectName, _appEnvironment.Wrapper.JsonSerializer);
-                _loadedPathsChanged = false;
-            }
-
-            GUILayout.Space(20);
-
-            // Scroll view for loaded paths
-            GUILayout.Label(ELT("loaded_files"), EditorStyles.boldLabel);
-            _scrollPos = EditorGUILayout.BeginScrollView(_scrollPos, GUILayout.Height(50));
-            foreach (var item in _loadedPaths)
-            {
-                EditorGUILayout.BeginHorizontal();
-                EditorGUILayout.SelectableLabel(item, rightAlignedStyle, GUILayout.Height(15));
-                if (GUILayout.Button(ELT("copy"), GUILayout.Width(50), GUILayout.Height(15)))
-                {
-                    EditorGUIUtility.systemCopyBuffer = item;
-                    Debug.Log("Copied: " + item);
-                }
-                EditorGUILayout.EndHorizontal();
-            }
-            EditorGUILayout.EndScrollView();
-            GUILayout.Space(5);
-            if (GUILayout.Button(ELT("load_new")))
-            {
-                LoadNewFile();
-            }
+            _loadFilesHelper.CreateLoadedPathsPanel();
 
             GUILayout.Space(20);
 
@@ -143,27 +123,27 @@ namespace PapayaModdingTool.Assets.Script.Editor.TextureModding
             EditorGUI.EndDisabledGroup();
         }
 
-        private void LoadNewFile()
-        {
-            LoadFileInfo? loadInfo = _projectWriter.LoadNewFile(ProjectName,
-                                        _appEnvironment.Wrapper.FileBrowser,
-                                        _appEnvironment.Wrapper.JsonSerializer,
-                                        LoadType.Texture);
-            if (loadInfo == null)
-                return;
+        // private void LoadNewFile()
+        // {
+        //     LoadFileInfo? loadInfo = _projectWriter.LoadNewFile(ProjectName,
+        //                                 _appEnvironment.Wrapper.FileBrowser,
+        //                                 _appEnvironment.Wrapper.JsonSerializer,
+        //                                 LoadType.Texture);
+        //     if (loadInfo == null)
+        //         return;
 
-            AttemptToLoadTexture((LoadFileInfo)loadInfo);
-            _loadedPathsChanged = true;
-        }
+        //     AttemptToLoadTexture((LoadFileInfo)loadInfo);
+        //     _loadedPathsChanged = true;
+        // }
 
-        private void AttemptToLoadTexture(LoadFileInfo loadInfo)
-        {
-            string textureDir = PredefinedPaths.PapayaTextureDir;
-            string textureSaveDir = Path.Combine(textureDir, ProjectName, loadInfo.folder);
-            if (!Directory.Exists(textureSaveDir))
-                Directory.CreateDirectory(textureSaveDir);
-            _textureAssetsLoader.LoadTextureAssets(loadInfo, ProjectName, textureSaveDir, _ppu);
-        }
+        // private void AttemptToLoadTexture(LoadFileInfo loadInfo)
+        // {
+        //     string textureDir = PredefinedPaths.PapayaTextureDir;
+        //     string textureSaveDir = Path.Combine(textureDir, ProjectName, loadInfo.folder);
+        //     if (!Directory.Exists(textureSaveDir))
+        //         Directory.CreateDirectory(textureSaveDir);
+        //     _textureAssetsLoader.LoadTextureAssets(loadInfo, ProjectName, textureSaveDir, _ppu);
+        // }
 
         private void RemoveTypedFile()
         {
